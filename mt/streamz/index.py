2043e278c864f42d499ea4b82ae0634e60521ea2
Minh-Tri Pham <pmtri80@gmail.com>
2020-09-03 08:01:18 +0100

    v0.0.2: knuth's hash_int()

--- mt/streamz/index.py
+++ mt/streamz/index.py
@@ -1,7 +1,25 @@
 '''Utitilites related to indexing.'''
 
 
-__all__ = ['IndexGrouping', 'on_list']
+__all__ = ['IndexGrouping', 'on_list', 'hash_int']
+
+
+def hash_int(i):
+    '''Knuth's cheap integer hash.
+
+    `Source <https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key>`_
+
+    Parameters
+    ----------
+    i : int
+        an integer to hash
+    
+    Returns
+    -------
+    int
+        an unsigned 32-bit hashed integer
+    '''
+    return int(i*2654435761 & 0x7FFFFFFF)
 
 
 class IndexGrouping(object):
