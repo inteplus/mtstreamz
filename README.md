770f57a5ecc379d3d212d6c53a4449592fb19844
Minh-Tri Pham <pmtri80@gmail.com>
2020-09-03 07:33:29 +0100

    v0.0.1: import from mtbase

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mtstreamz
 Minh-Tri's extension of the streamz Python library.
+
+## Documentation
+
+Check out [mtdoc](https://mtdoc.readthedocs.io/)
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_namespace_packages
+from mt.streamz.version import version
+
+setup(name='mtstreamz',
+      version=version,
+      description="Minh-Tri Pham's extension of the streamz Python library",
+      author=["Minh-Tri Pham"],
+      packages=find_namespace_packages(include=['mt.*']),
+      install_requires=[
+          'tornado>=6.0.4', # 2020/08/27: tornado 6 is needed and distributed must be >= v2.24.0
+          'dask>=2.24.0', # for simple multiprocessing jobs
+          'distributed>=2.24.0',  # for simple multiprocessing jobs
+          'streamz>=0.5.5', # for streaming
+      ],
+      url='https://github.com/inteplus/mtstreamz',
+      project_urls={
+          'Documentation': 'https://mtdoc.readthedocs.io/en/latest/mt.streamz/mt.streamz.html',
+          'Source Code': 'https://github.com/inteplus/mtstreamz',
+          }
+      )
