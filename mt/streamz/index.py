f47bb50d43f558ba9a4d0383aab5152f842a225e
Minh-Tri Pham <pmtri80@gmail.com>
2020-09-12 00:55:15 +0100

    v0.0.7: index.py: item_cnt can be None to represent infinity

--- mt/streamz/index.py
+++ mt/streamz/index.py
@@ -27,17 +27,17 @@ class IndexGrouping(object):
 
     Parameters
     ----------
-    item_cnt : int
-        number of items in the collection
+    item_cnt : int or None
+        number of items in the collection. If None is provided, item_cnt is assumed infinity.
     max_group_size : int
         maximum number of items per group
     policy : {'sequential', 'repeated'}
-        policy to partition the collection. Value 'sequential' means the collection is grouped sequentially into `[0, max_group_size), [max_group_size, max_group_size*2), ..., [X, item_cnt)`. Value 'repeated' means the items can be repeated beyond `item_cnt`.
+        policy to partition the collection. Value 'sequential' means the collection is grouped sequentially into `[0, max_group_size), [max_group_size, max_group_size*2), ..., [X, item_cnt)`. If item_cnt is None, then we assume item_cnt is infinity. Value 'repeated' means the items can be repeated beyond `item_cnt` and rolled back to [0, item_cnt) range by taking moduli.
     '''
     def __init__(self, item_cnt, max_group_size, policy='sequential'):
 
-        if not isinstance(item_cnt, int) or item_cnt <= 0:
-            raise ValueError("Argument 'item_cnt' must be a positive integer, but {} was provided.".format(item_cnt))
+        if (item_cnt is not None) and (not isinstance(item_cnt, int) or item_cnt <= 0):
+            raise ValueError("Argument 'item_cnt' must be None or a positive integer, but {} was provided.".format(item_cnt))
         self.item_cnt = item_cnt
         
         if not isinstance(max_group_size, int) or max_group_size <= 0:
@@ -49,9 +49,9 @@ class IndexGrouping(object):
         self.policy = policy
 
     def group_count(self):
-        '''Returns the number of groups, or None if 'repeated' policy was provided.'''
+        '''Returns the number of groups, or None if either 'repeated' policy was provided or ('sequential' policy was provided and 'item_cnt' was None).'''
         if self.policy == 'sequential':
-            return (self.item_cnt + self.max_group_size - 1) // self.max_group_size
+            return None if self.item_cnt is None else (self.item_cnt + self.max_group_size - 1) // self.max_group_size
         if self.policy == 'repeated':
             return None
 
@@ -71,6 +71,11 @@ class IndexGrouping(object):
         if not isinstance(group_id, int) or group_id < 0:
             raise ValueError("Argument 'group_id' must be a non-negative integer, but {} was provided.".format(group_id))
 
+        if self.item_cnt is None:
+            start_id = group_id*self.max_group_size
+            end_id = (group_id+1)*self.max_group_size
+            return [x for x in range(start_id, end_id)]
+
         if self.policy == 'sequential':
             start_id = group_id*self.max_group_size
             if start_id >= self.item_cnt:
@@ -81,7 +86,11 @@ class IndexGrouping(object):
         if policy=='repeated':
             start_id = group_id*self.max_group_size
             end_id = (group_id+1)*self.max_group_size
-            return [x%item_cnt for x in range(start_id, end_id)]
+            return [x%self.item_cnt for x in range(start_id, end_id)]
+
+    def __call__(self, group_id):
+        return self.get_indices(group_id)
+    __call__.__doc__ = get_indices.__doc__
 
 
 def on_list(func):
