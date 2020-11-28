51e5f6905999389bc34dca74785fb575718deabf
Minh-Tri Pham <pmtri80@gmail.com>
2020-11-28 09:44:38 +0000

    v0.1.1: code clean up

--- mt/streamz/index.py
+++ mt/streamz/index.py
@@ -2,8 +2,8 @@
 
 
 from mt.base.deprecated import deprecated_func
-import mt.base.functional as _bf
-import mt.base.hashing as _bh
+from mt.base.functional import on_list, join_funcs
+from mt.base.hashing import hash_int
 
 
 __all__ = ['IndexGrouping']
@@ -78,58 +78,3 @@ class IndexGrouping(object):
     def __call__(self, group_id):
         return self.get_indices(group_id)
     __call__.__doc__ = get_indices.__doc__
-
-
-@deprecated_func("0.0.8", suggested_func="mt.base.hashing.hash_int", removed_version="0.2.0", docstring_prefix="    ")
-def hash_int(i):
-    '''Knuth's cheap integer hash.
-
-    `Source <https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key>`_
-
-    Parameters
-    ----------
-    i : int
-        an integer to hash
-    
-    Returns
-    -------
-    int
-        an unsigned 32-bit hashed integer
-    '''
-    return _bh.hash_int(i)
-
-
-@deprecated_func("0.0.8", suggested_func="mt.base.functional.on_list", removed_version="0.2.0", docstring_prefix="    ")
-def on_list(func):
-    '''Turns a function that operates on each element at a time into a function that operates on a colletion of elements at a time. Can be used as a decorator.
-
-    Parameters
-    ----------
-    func : function
-        the function to build upon. Its expected form is `def func(x, *args, **kwargs) -> object`.
-
-    Returns
-    -------
-    function
-        a wrapper function `def func_on_list(list_x, *args, **kwargs) -> list_of_objects` that invokes `func(x, *args, **kwargs)` for each x in list_x.
-    '''
-    return _bf.on_list(func)
-
-
-@deprecated_func("0.0.8", suggested_func="mt.base.functional.join_funcs", removed_version="0.2.0", docstring_prefix="    ")
-def join_funcs(*funcs, left_to_right=True):
-    '''Concatenate functions taking a single argument into a single function.
-
-    Parameters
-    ----------
-    *funcs : list
-        list of functions f1(x), f2(x), ..., fn(x)
-    left_to_right : boolean
-        whether to concatenate from left to right or from right to left.
-
-    Returns
-    -------
-    func : function
-        The function `f(x) = fn(...f2(f1(x))...)` if left_to_right is True, else `f(x) = f1(f2(...fn(x)...))`.
-    '''
-    return _bf.join_funcs(*funcs, left_to_right=left_to_right)
